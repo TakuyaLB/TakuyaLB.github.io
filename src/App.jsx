70137f2d94a3c225f4dae82e35683d60b5e77546
TakuyaLB <tlb2160@columbia.edu>
2026-09-10 14:44:09 -0400

    isaac sim image

--- src/App.jsx
+++ src/App.jsx
@@ -22,7 +22,7 @@ const Portfolio = () => {
     {
       id: 3,
       title: "Space Robotics",
-      image: "/work/ADSS_CAD_gripper_visible.png",
+      image: "/work/ADSS_isaac_sim.png",
       projectId: 3,
     },
   ];
